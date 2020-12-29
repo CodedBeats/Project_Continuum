e68a60cf8f5cb4aad74860a1a62f3b59772d865f
Luca Haar <luca.haar@icloud.com>
2020-12-29 20:49:02 +1100

    initialized the canvas_welcome feature

--- canvas_welcome/index.js
+++ canvas_welcome/index.js
@@ -7,6 +7,10 @@ const client = new DiscordJS.Client()
 //listen for bot activation
 client.on("ready", () => {
     console.log("Vamanos")
+
+    new WOKCommands(client, "commands", "features")
+        .setMongoPath(process.env.MONGO_URI)
+        .setDefaultPrefix("!")
 })
 
 client.login(process.env.TOKEN)
