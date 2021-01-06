c5bece6b329cfb7a5532558b9c5c2307f24e66f0
Luca Haar <luca.haar@icloud.com>
2021-01-06 16:43:37 +1100

    starting economy system

--- Canvas-Welcome/commands/setwelcome.js
+++ Canvas-Welcome/commands/setwelcome.js
@@ -3,9 +3,12 @@ const welcomeSchema = require("../models/welcome-schema")
 
 const cache = new Map()
 
+// An async function to load the data
 const loadData = async () => {
+    // Get all stored channel IDs
     const results = await welcomeSchema.find()
 
+    // Loop through them all and set them in our map
     for (const result of results ) {
         cache.set(result._id, result.channelID)
     }
@@ -18,8 +21,10 @@ module.exports = {
         "ADMINISTRATOR"
     ],
     callback: async (message) => {
+        // Destructure the guild and channel properties from the message object
         const { guild, channel } = message
 
+        // Use find one and update to either update or insert the data depending on if it exists already
         await welcomeSchema.findOneAndUpdate(
             {
                 _id: guild.id,
@@ -33,6 +38,7 @@ module.exports = {
             }
         )
 
+        // Store the information in the cache
         cache.set(guild.id, channel.id)
 
         message.reply("Welcome Channel Set!")
