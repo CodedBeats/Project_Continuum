a7f04ce2940b58be25a44d346b69f9653315d2bc
Luca Haar <luca.haar@icloud.com>
2021-03-09 18:59:27 +1100

    added a vc channel that displays the server's members live

--- Helpful-Commands/commands/counters.js
+++ Helpful-Commands/commands/counters.js
@@ -5,14 +5,20 @@ module.exports = client => {
         if (message.content.toLowerCase() === "$member count") {
             let serverID = message.guild.id;
             let guild = client.guilds.cache.get(serverID)
-            message.channel.send(`The ${message.guild.name} has ${guild.memberCount} members`)
+            let members = guild.memberCount // - 5
+            // let onlineMemberCount = guild.members.filter(m => m.presence.status === "online").size     * .filter wont work *
+            // console.log(onlineMemberCount)
+            // message.channel.send(`The ${message.guild.name} has ${members} members`)
             console.log("Server member count")
-        }
-    })
 
+            const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("Server's Member", '')
+            .setDescription(`The ${message.guild.name} has ${members} members`)
+            message.channel.send(embed);
+        } 
 
-    client.on("message", message => {
-        if (message.content.toLowerCase() === "$bot servers") {
+        else if (message.content.toLowerCase() === "$bot servers") {
             
             let serverlist = ''
             client.guilds.cache.forEach((guild) => {
@@ -26,6 +32,7 @@ module.exports = client => {
             message.channel.send(embed);
         }
     })
+
 
 }
 
--- Helpful-Commands/index.js
+++ Helpful-Commands/index.js
@@ -11,11 +11,43 @@ const counters = require("./commands/counters");
 client.on("ready", () => {
     console.log("Be Helpful")
 
+
+    // initialize live member count channel
+    let myGuid = client.guilds.cache.get("731399692868649030")
+    let myMemberCount = myGuid.memberCount
+    // console.log(myMemberCount)
+    let memberCountChannel = myGuid.channels.cache.get("818751101557407754")
+    // console.log(memberCountChannel.name)
+    memberCountChannel.setName("Members: " + myMemberCount)
+    .then(result => console.log("Guild Mmembers Is Live"))
+    .catch(error => console.log(error))
+
+
     // initizlize apropriate files
     ping(client)
     counters(client)
-
-
 })
+
+
+// Moderating live member count
+client.on("guildMemberAdd", member => {
+    let myGuid = client.guilds.cache.get("731399692868649030")
+    let myMemberCount = myGuid.memberCount
+    let memberCountChannel = myGuid.channels.cache.get("818751101557407754")
+    memberCountChannel.setName("Members: " + myMemberCount)
+    .then(result => console.log("Gained a guild member"))
+    .catch(error => console.log(error))
+})
+
+client.on("guildMemberRemove", member => {
+    let myGuid = client.guilds.cache.get("731399692868649030")
+    let myMemberCount = myGuid.memberCount
+    let memberCountChannel = myGuid.channels.cache.get("818751101557407754")
+    memberCountChannel.setName("Members: " + myMemberCount)
+    .then(result => console.log("Lost a guild member"))
+    .catch(error => console.log(error))
+})
+
+
 
 client.login(process.env.TOKEN)
