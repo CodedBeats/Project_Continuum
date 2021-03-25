0392a4535a38730e4e021fd406b6fc5401f7fabf
Luca Haar <luca.haar@icloud.com>
2021-03-25 18:28:48 +1100

    added music function files

---
+++ Music-System/Music/stop.js
@@ -0,0 +1,7 @@
+module.exports = {execute(message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end();
+	},
+};
---
+++ Music-System/index.js
@@ -0,0 +1,37 @@
+// Dependencies
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+require('dotenv').config();
+const prefix = "-"
+
+// initialize bot
+const client = new Discord.Client();
+
+
+// initialize files
+const baseCommands = require("./Music/base_commands")
+
+
+client.once('ready', () => {
+    console.log('Groovy');
+
+    baseCommands(client)
+});
+
+
+client.once('reconnecting', () => {
+    console.log('Reconnecting!');   
+});
+client.once('disconnect', () => {
+    console.log('Disconnect!');
+});
+
+
+
+
+
+
+
+
+
+client.login(process.env.TOKEN)
