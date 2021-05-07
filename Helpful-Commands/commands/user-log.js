6e59b51fcf7e1d7ac5f97368613762a52ee015fb
Luca Haar <luca.haar@icloud.com>
2021-05-08 00:05:02 +1000

    finished the selected user's avatar command

--- Helpful-Commands/commands/user-log.js
+++ Helpful-Commands/commands/user-log.js
@@ -25,14 +25,18 @@ module.exports = client => {
                 return;
             }
 
-            let user = message.mentions.users.first();
-            let userAvatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.jpeg`
+            let user = message.mentions.users.first()
+            let userAvatarURL = user.displayAvatarURL({dynamic : true, size: 2048})
+            let userAvatar = user.displayAvatarURL({dynamic : true, size: 1024}); 
             let avatarEmbed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setAuthor(user.username)
+            .setDescription(`**[Avatar URL](${userAvatarURL})**`)
             .setImage(userAvatar);
 
             message.channel.send(avatarEmbed);
+            console.log("Selected User's Avatar")
+            // console.log(userAvatarURL)
         }
     })
 
